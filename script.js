d68cc738c439b8787b1a879e0172e5434c395ef5
sy2581005-ui <sy2581005@gmail.com>
2026-07-01 04:08:58 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ return email.includes("@")? true : false
 
 console.log (isvalidEmail ("@.com"));
 isvalidEmail;
+
+console.log (isvalidEmail ("@.com"));
+isvalidEmail;
+
+
+const downloadBtn = document.querySelector('.hero-buttons .btn-primary');
+
+downloadBtn.addEventListener('click', function(event) {
+    event.preventDefault(); 
+
+    const cvUrl = 'images/my-cv.jpeg'; 
+    
+    const tempLink = document.createElement('a');
+    tempLink.href = cvUrl;
+    
+    
+    tempLink.download = 'My_Professional_CV.pdf'; 
+    
+    
+    document.body.appendChild(tempLink);
+    tempLink.click();
+    document.body.removeChild(tempLink);
+});
